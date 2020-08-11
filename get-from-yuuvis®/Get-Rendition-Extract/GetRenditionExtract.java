515d2d0c28a40e95e560c13089826f9456332465
Max Lüdemann <luedemann@optimal-systems.de>
2020-08-11 09:30:58 +0200

    add extraction rendition call

---
+++ get-from-yuuvis®/Get-Rendition-Extract/GetRenditionExtract.java
@@ -0,0 +1,41 @@
+import okhttp3.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetRenditionExtract
+{
+    public static final String BASEURL = "https://api.yuuvis.io"+"/dms-view";
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final MediaType PLAINTEXT = MediaType.parse("text/plain; charset=utf-8");
+    public static final MediaType XML = MediaType.parse("application/xml; charset=utf-8");
+
+    public static void main(String[] args)
+    {
+        Map<String, String> headerMap = new HashMap<>();
+        String key = "";
+        String objectId = "";
+
+        headerMap.put("Ocp-Apim-Subscription-Key", key);
+
+        try
+        {
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            OkHttpClient client = builder.build();
+
+            Request request = null;
+            Headers headers = Headers.of(headerMap);
+
+            request = new Request.Builder().headers(headers).url(BASEURL + "/objects/" + objectId + "/contents/renditions/extract").get().build();
+
+            Response response = client.newCall(request).execute();
+            System.out.println(response.toString());
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
